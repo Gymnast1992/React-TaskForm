2eff86707b8f92f2de22b1266e1fef05acb735ae
Maksym Semiankiv <1992maksym1992@gmail.com>
2025-01-25 18:17:52 +0100

    set task and status section

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import "./TaskForm.css";
 import Tag from './Tag';
 
 const TaskForm = () => {
+  const [taskData, setTaskData] = useState({
+      task: "",
+      status: "todo"
+  });
+
+  const handleChange = (e) => {
+      const {name, value} = e.target;
+
+      setTaskData(prev => {
+        return {...prev, [name]: value}
+      })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(taskData);
+  }
   return (
     <header className='app_header'>
-        <form>
-            <input type="text" className="task_input" placeholder='Enter your task'/>
+        <form onSubmit={handleSubmit}>
+            <input type="text"
+                   name="task" 
+                   className="task_input" 
+                   placeholder='Enter your task'
+                   onChange={handleChange}/>
 
             <div className='task_form_bottom_line'>
               <div>
@@ -17,7 +38,9 @@ const TaskForm = () => {
                 <Tag tagName="React" />
               </div>
               <div>
-                <select className="task_status">
+                <select name="status"
+                        className="task_status" 
+                        onChange={handleChange}>
                     <option value="todo">To Do</option>
                     <option value="doing">Doing</option>
                     <option value="done">Done</option>
